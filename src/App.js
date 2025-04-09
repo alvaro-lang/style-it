ed5528d19bf28d8a1a28de1aa8af23625cf3f566
Alvaro Pastor Sellers <alvaropers96@gmail.com>
2025-04-09 16:16:19 +0100

    added logo and some styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import './App.css';
+import './styles/App.css';
 
 function App() {
   return (
     <div id="app">
       <header className="header">
+        <img className='page-logo' src="./images/page_logo_white.png" alt="Page Logo"></img>
       </header>
+      <div className='page'>
+      </div>
     </div>
   );
 }
